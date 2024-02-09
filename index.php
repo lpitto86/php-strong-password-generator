2dc2c36b85b0da764fe090ae739e9c08be3ac43d
lpitto86 <luca.p481@gmail.com>
2024-02-09 15:47:49 +0100

    Inizializzazione

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Password Generator</title>
+
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      </head>
+      <body>
+            <header>
+                  <h1>
+                        <div class="container">
+                              <div class="row">
+                                    <div class="col">
+                                          <h1>
+                                                PHP Strong Password Generator
+                                          </h1>
+                                    </div>
+                              </div>
+                        </div>
+                  </h1>
+            </header>
+            <main>
+                  <div class="container">
+                        <div class="row">
+                              <div class="col">
+                                    <form class="row">
+                                          <div class="mb-3">
+                                                <label for="lenght" class="visually-hidden">Lunghezza Password</label>
+                                                <input type="number" class="form-control" id="lenght" placeholder="Inserisci la lunghezza della Password...">
+                                          </div>
+                                          <div>
+                                                <button type="submit" class="btn btn-primary mb-3">
+                                                      Genera Password
+                                                </button>
+                                          </div>
+                                    </form>
+                              </div>
+                        </div>
+                  </div>
+            </main>
+            <footer>
+            </footer>
+      </body>
+</html>
